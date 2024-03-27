11447ef49ced0bbece62a49d7dffd15048599ca0
Ashley <145135067+AshleyQian@users.noreply.github.com>
2024-03-27 01:19:19 -0400

    Update main.c

--- ESE5160/main.c
+++ ESE5160/main.c
@@ -14,14 +14,14 @@
 
 #define UART_MODULE       SERCOM4
 #define UART_SERCOM_MUX   USART_RX_3_TX_2_XCK_3
-//#define UART_SERCOM_GCLK  SERCOM4_GCLK_ID_CORE
+#define UART_SERCOM_GCLK  SERCOM4_GCLK_ID_CORE
 #define BUFFER_SIZE 64
 
 #define UART_TX_PIN PINMUX_PB10D_SERCOM4_PAD2 //SAM W25 TX line
 #define UART_RX_PIN PINMUX_PB11D_SERCOM4_PAD3 //SAM W25 RX line
 
 static uint8_t rx_buffer[BUFFER_SIZE];
-static uint8_t tx_buffer[BUFFER_SIZE] = "UART Data!\r\n";
+static uint8_t tx_buffer[BUFFER_SIZE] = "UART!\r\n";
 
 static struct usart_module usart_instance;
 static int receive_flag = 0;
@@ -63,15 +63,14 @@ int main (void)
 	delay_init();
 	configure_uart();
 
-	//usart_write_buffer_job(&usart_instance, tx_buffer, strlen((const char *)tx_buffer));
-	usart_write_buffer_job(&usart_instance, tx_buffer, sizeof(tx_buffer));
+	usart_write_buffer_job(&usart_instance, tx_buffer, strlen((const char *)tx_buffer));
 
 	while (1) {
 		/* Is button pressed? */
 		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
 			/* Yes, send UART data packet. */
-			//usart_write_buffer_job(&usart_instance, tx_buffer, strlen((const char *)tx_buffer));
-			usart_write_buffer_job(&usart_instance, tx_buffer, sizeof(tx_buffer));
+			receive_flag = 0;
+			usart_write_buffer_job(&usart_instance, tx_buffer, strlen((const char *)tx_buffer));
 		}
 		/*if the data has been received, blink the on board LED*/
 		if (receive_flag == 1){
